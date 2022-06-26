7866f8acf8384e05df14b9916ca45b91dafb411f
Cola0165 <zwq1773677992@sina.com>
2022-06-26 13:55:18 +0800

    init

---
+++ HDBMMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HDBMMVC.Startup))]
+namespace HDBMMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
